102879abca6a0c20dc4e8aabbb6afbb8aa1c3456
Amy Miller <amymiller@192.168.1.116>
2021-12-30 22:50:51 +0000

    initial components added

---
+++ src/Components/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// This is List Screen
+
+type Props = {};
+
+const Home: React.FC<Props> = () => {
+	return <div>List View 2</div>;
+};
+
+export default Home;
---
+++ src/Components/Randomize.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+const Randomize = () => {
+	return <></>;
+};
+
+export default Randomize;
