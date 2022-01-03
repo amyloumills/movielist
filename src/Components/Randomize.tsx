baf6379b8d88d4e60ce4cea11a9e3589df1b3612
Amy Miller <amymiller@192.168.1.116>
2022-01-03 21:00:40 +0000

    fixed state error

--- src/Components/Randomize.tsx
+++ src/Components/Randomize.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { MoviesProps } from '../types';
 
-type Props = MoviesProps;
+type Props = MoviesProps & {};
 
-const Randomize: React.FC<Props> = () => {
-	return <></>;
+const Randomize: React.FC<Props> = ({ movies }) => {
+	const movie = movies[0];
+	return movie ? <div>{movie.label}</div> : <div>No movie found</div>;
 };
 
 export default Randomize;
